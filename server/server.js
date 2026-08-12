a5f80c2384a30bc0d9b56e298c675bcd1fe05b26
TaqiMasum <taqimasum@192.168.1.54>
2026-08-12 22:17:56 +0600

    coach, type

--- server/server.js
+++ server/server.js
@@ -110,7 +110,74 @@ app.get("/api/train_details/:train_id", async(req, res)=>{
     JOIN station s on rs.station_id = s.station_id
     WHERE train_id = $1
     `;
+    const qTypes = `
+    WITH journey_details AS (
+    SELECT 
+        sc.schedule_id,
+        t.route_id,
+        (SELECT sequence_no FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $2 AND rs.route_id = t.route_id) as start_seq,
+        (SELECT sequence_no FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $4 AND rs.route_id = t.route_id) as end_seq,
+        (SELECT distance_km FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $2 AND rs.route_id = t.route_id) as start_distance,
+        (SELECT distance_km FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $4 AND rs.route_id = t.route_id) as end_distance
+    FROM train t
+    JOIN schedule sc ON sc.train_id = t.train_id
+    WHERE t.train_id = $3 AND sc."date" = $1
+    ),
+    seat_statuses AS (
+        SELECT 
+            c.type as coach_type,
+            s.seat_id,
+            fr.base_fare + (fr.rate_per_km * (jd.end_distance - jd.start_distance)) as calculated_price,
+            CASE 
+                -- 1. Check if booked
+                WHEN EXISTS (
+                    SELECT 1 FROM ticket_seat ts
+                    WHERE ts.seat_id = s.seat_id
+                      AND ts.schedule_id = jd.schedule_id
+                      AND int4range(ts.from_seq, ts.to_seq) && int4range(jd.start_seq, jd.end_seq)
+                ) THEN 'booked'
 
+                -- 2. Check if locked (pending)
+                WHEN EXISTS (
+                    SELECT 1 FROM seat_lock sl
+                    WHERE sl.seat_id = s.seat_id
+                      AND sl.schedule_id = jd.schedule_id
+                      AND sl.status = 'active'
+                      AND sl.expires_at > NOW()
+                      AND int4range(sl.from_seq, sl.to_seq) && int4range(jd.start_seq, jd.end_seq)
+                ) THEN 'pending'
+
+                ELSE 'available'
+            END AS status
+        FROM seat s
+        JOIN coach c ON s.coach_id = c.coach_id
+        JOIN fare_rate fr ON fr.seat_type = c.type
+        LEFT JOIN journey_details jd ON true
+        WHERE c.train_id = $3
+    )
+    SELECT 
+        coach_type,
+        ROUND(MAX(calculated_price),2) AS price,
+        COUNT(*) FILTER (WHERE status = 'available') AS available_count,
+        COUNT(*) FILTER (WHERE status = 'booked') AS booked_count,
+        COUNT(*) FILTER (WHERE status = 'pending') AS pending_count
+    FROM seat_statuses
+    GROUP BY coach_type;
+    `;
+    const qCoaches = `
+    SELECT t.train_name, c.coach_name
+    FROM coach c
+    JOIN train t ON c.train_id = t.train_id
+    WHERE t.train_id = $1
+    `;
     const qSeats = `
     WITH journey_details AS (
     SELECT 
@@ -121,7 +188,13 @@ app.get("/api/train_details/:train_id", async(req, res)=>{
          WHERE st.station_name = $2 AND rs.route_id = t.route_id) as start_seq,
         (SELECT sequence_no FROM route_station rs 
          JOIN station st ON rs.station_id = st.station_id 
-         WHERE st.station_name = $4 AND rs.route_id = t.route_id) as end_seq
+         WHERE st.station_name = $4 AND rs.route_id = t.route_id) as end_seq,
+        (SELECT distance_km FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $2 AND rs.route_id = t.route_id) as start_distance,
+        (SELECT distance_km FROM route_station rs 
+         JOIN station st ON rs.station_id = st.station_id 
+         WHERE st.station_name = $4 AND rs.route_id = t.route_id) as end_distance
     FROM train t
     JOIN schedule sc ON sc.train_id = t.train_id
     WHERE t.train_id = $3 AND sc."date" = $1
@@ -130,6 +203,8 @@ app.get("/api/train_details/:train_id", async(req, res)=>{
         s.seat_id, 
         s.seat_number, 
         c.coach_name,
+        c.type, 
+        fr.base_fare + (fr.rate_per_km * (jd.end_distance - jd.start_distance)) as calculated_price,
         CASE 
             WHEN EXISTS (
                 SELECT 1 FROM ticket_seat ts
@@ -153,17 +228,22 @@ app.get("/api/train_details/:train_id", async(req, res)=>{
         END AS segment_status
     FROM seat s
     JOIN coach c ON s.coach_id = c.coach_id
-    CROSS JOIN journey_details jd
+    JOIN fare_rate fr ON fr.seat_type = c.type
+    LEFT JOIN journey_details jd ON TRUE
     WHERE c.train_id = $3;
   `;
     // const Route  = await pool.query(qRoute, [train_id]);
 
-    const [Route, Seats] = await Promise.all([
+    const [Route, Coaches, Seats, Types] = await Promise.all([
       pool.query(qRoute, [train_id]),
-      pool.query(qSeats, [date, from,train_id,to])
+      pool.query(qCoaches, [train_id]),
+      pool.query(qSeats, [date, from,train_id,to]),
+      pool.query(qTypes, [date, from,train_id,to])
     ]);
     res.json({
       route: Route.rows,
+      types: Types.rows,
+      coaches: Coaches.rows,
       seats: Seats.rows
     });
   } catch (err) {
